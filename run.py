fef882ca061b1e6eebde442ddd22d49e0c76aaec
sinicto <lahktin.ia@phystech.edu>
2023-04-06 01:23:05 +0300

    add run

---
+++ run.py
@@ -0,0 +1,16 @@
+from segmentation import Unet, Loader
+
+loader = Loader()
+train, dev, test = loader.load_split_data()
+model = Unet()
+model.fit(train)
+model.store()
+
+print('Train')
+model.evaluate(train)
+
+print('Dev')
+model.evaluate(dev)
+
+print('Test')
+model.evaluate(test)
