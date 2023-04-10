a54d3661c9eef276bb2f3e771a8257a03ef98642
sinicto <lahktin.ia@phystech.edu>
2023-04-11 00:47:29 +0300

    test

--- examples/run.py
+++ examples/run.py
@@ -1,7 +1,10 @@
 from segmentation import Unet, Loader
 
 print('='*4, 'START')
-loader = Loader()
+default_data_path = 'drive/MyDrive/data/CVPPPSegmData/'
+default_split_path = 'drive/MyDrive/data/CVPPPSegmData/split.csv'
+loader = Loader(split_path=default_split_path,
+                 data_path=default_data_path)
 train, dev, test = loader.load_split_data()
 model = Unet()
 model.fit(train)
